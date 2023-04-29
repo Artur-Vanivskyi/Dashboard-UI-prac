b11f5b96a4408432df87b68d2a1b1da071d02f2a
Artur Vanivskyi <vanivskyi.artur@gmail.com>
2023-04-29 19:54:56 -0400

    added functionality onclick for navbar top page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -22,20 +22,21 @@ const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
       style={{ color }}
       className="relative text-xl rounded-full p-3 hover:bg-light-gray"
     >
-      <span style={{background: dotColor}} className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2">
-{icon}
+      <span
+        style={{ background: dotColor }}
+        className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2"
+      >
+        {icon}
       </span>
     </button>
   </TooltipComponent>
 );
 
 const Navbar = () => {
-  const { activeMenu, setActiveMenu } = useStateContext();
+  const { activeMenu, setActiveMenu, isClicked, setIsClicked, handleClick } =
+    useStateContext();
 
-
-  const handleClick = () => {
-
-  }
+ 
 
   return (
     <div className="flex justify-between p-2 md:mx-6 relative">
@@ -46,37 +47,46 @@ const Navbar = () => {
         icon={<AiOutlineMenu />}
       />
       <div className="flex">
-      <NavButton
-        title="Cart"
-        customFunc={() => handleClick('cart')}
-        color="blue"
-        icon={<FiShoppingCart />}
-      />
-      <NavButton
-        title="Chat"
-        dotColor="#03C9D7"
-        customFunc={() => handleClick('chat')}
-        color="blue"
-        icon={<BsChatLeft />}
-      />
-      <NavButton
-        title="Notifications"
-        dotColor="#03C9D7"
-        customFunc={() => handleClick('notification')}
-        color="blue"
-        icon={<RiNotification3Line />}
-      />
-      <TooltipComponent content="Profile" position="BottomCenter">
-        <div className="flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg" onClick={()=> handleClick('userProfile')}>
-          <img src={avatar} alt="avatar" className="rounded-full w-8 h-8" />
-          <p>
-            <span className="text-gray-400 text014">Hi, </span> {' '}
-            <span className="text-gray-400 font-bold ml-1 text-14">Michael</span>
-          </p>
-          <MdKeyboardArrowDown className="text-gray-400 text014"/>
-        </div>
+        <NavButton
+          title="Cart"
+          customFunc={() => handleClick("cart")}
+          color="blue"
+          icon={<FiShoppingCart />}
+        />
+        <NavButton
+          title="Chat"
+          dotColor="#03C9D7"
+          customFunc={() => handleClick("chat")}
+          color="blue"
+          icon={<BsChatLeft />}
+        />
+        <NavButton
+          title="Notifications"
+          dotColor="#03C9D7"
+          customFunc={() => handleClick("notification")}
+          color="blue"
+          icon={<RiNotification3Line />}
+        />
+        <TooltipComponent content="Profile" position="BottomCenter">
+          <div
+            className="flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg"
+            onClick={() => handleClick("userProfile")}
+          >
+            <img src={avatar} alt="avatar" className="rounded-full w-8 h-8" />
+            <p>
+              <span className="text-gray-400 text014">Hi, </span>{" "}
+              <span className="text-gray-400 font-bold ml-1 text-14">
+                Michael
+              </span>
+            </p>
+            <MdKeyboardArrowDown className="text-gray-400 text014" />
+          </div>
+        </TooltipComponent>
 
-      </TooltipComponent>
+        {isClicked.cart && <Cart />}
+        {isClicked.chat && <Chat />}
+        {isClicked.userProfile && <UserProfile />}
+        {isClicked.notification && <Notification />}
       </div>
     </div>
   );
--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -11,10 +11,19 @@ const initialState = {
 
 export const ContextProvider = ({ children }) => {
   const [activeMenu, setActiveMenu] = useState(true);
-  const [] = useState();
+  const [isClicked, setIsClicked] = useState(initialState);
+
+  const handleClick = (clicked) => {
+    setIsClicked({
+      ...initialState,
+      [clicked]: true,
+    });
+  };
 
   return (
-    <StateContext.Provider value={{ activeMenu, setActiveMenu }}>
+    <StateContext.Provider
+      value={{ activeMenu, setActiveMenu, isClicked, setIsClicked, handleClick }}
+    >
       {children}
     </StateContext.Provider>
   );

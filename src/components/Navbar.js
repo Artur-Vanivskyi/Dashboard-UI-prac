664927fc996c35263d37f5e12685ea79965fca9d
Artur Vanivskyi <vanivskyi.artur@gmail.com>
2023-04-29 19:45:57 -0400

    added layout of nav on top pf the page, cart, chat, notifications, user profile

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,7 +15,7 @@ import { Cart, Chat, Notification, UserProfile } from ".";
 import { useStateContext } from "../contexts/ContextProvider";
 
 const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
-  <TooltipComponent content="title" position="BottomCenter">
+  <TooltipComponent content={title} position="BottomCenter">
     <button
       type="button"
       onClick={customFunc}
@@ -32,6 +32,11 @@ const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
 const Navbar = () => {
   const { activeMenu, setActiveMenu } = useStateContext();
 
+
+  const handleClick = () => {
+
+  }
+
   return (
     <div className="flex justify-between p-2 md:mx-6 relative">
       <NavButton
@@ -40,6 +45,39 @@ const Navbar = () => {
         color="blue"
         icon={<AiOutlineMenu />}
       />
+      <div className="flex">
+      <NavButton
+        title="Cart"
+        customFunc={() => handleClick('cart')}
+        color="blue"
+        icon={<FiShoppingCart />}
+      />
+      <NavButton
+        title="Chat"
+        dotColor="#03C9D7"
+        customFunc={() => handleClick('chat')}
+        color="blue"
+        icon={<BsChatLeft />}
+      />
+      <NavButton
+        title="Notifications"
+        dotColor="#03C9D7"
+        customFunc={() => handleClick('notification')}
+        color="blue"
+        icon={<RiNotification3Line />}
+      />
+      <TooltipComponent content="Profile" position="BottomCenter">
+        <div className="flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg" onClick={()=> handleClick('userProfile')}>
+          <img src={avatar} alt="avatar" className="rounded-full w-8 h-8" />
+          <p>
+            <span className="text-gray-400 text014">Hi, </span> {' '}
+            <span className="text-gray-400 font-bold ml-1 text-14">Michael</span>
+          </p>
+          <MdKeyboardArrowDown className="text-gray-400 text014"/>
+        </div>
+
+      </TooltipComponent>
+      </div>
     </div>
   );
 };

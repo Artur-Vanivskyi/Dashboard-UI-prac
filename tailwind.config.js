bc41e05077c8e38179e358857b4a7ce55d99761a
Artur Vanivskyi <vanivskyi.artur@gmail.com>
2023-04-30 13:46:26 -0400

    finished ecom page, fixed background bug

--- tailwind.config.js
+++ tailwind.config.js
@@ -40,7 +40,7 @@ module.exports = {
         },
         backgroundImage: {
           'hero-pattern':
-            "url('https://i.ibb.co/MkvLDfb/Rectangle-4389.png')",
+            "url('https://www.kindpng.com/picc/m/732-7329685_e-commerce-website-background-image-e-commerce-website.png')",
         },
       },
     },

efa3ebf4131d3e1bfb06c141ba8bb07969b4927f
Artur Vanivskyi <vanivskyi.artur@gmail.com>
2023-05-01 15:39:56 -0400

    added change on theme change

--- src/pages/Ecommerce.js
+++ src/pages/Ecommerce.js
@@ -12,7 +12,10 @@ import {
 import { useStateContext } from "../contexts/ContextProvider";
 
 const Ecommerce = () => {
-  return (
+
+  const { currentColor } = useStateContext();
+
+   return (
     <div className="mt-24">
       <div className="flex flex-wrap lg:flex-nowrap justify-center">
         <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center">
@@ -26,7 +29,7 @@ const Ecommerce = () => {
           <div className="mt-6">
             <Button
               color="white"
-              bgColor="blue"
+              bgColor={currentColor}
               text="Download"
               borderRadius="10px"
               size="md"
@@ -98,20 +101,20 @@ const Ecommerce = () => {
               </div>
               <div className="mt-5">
                 <SparkLine
-                  currentColor="blue"
+                  currentColor={currentColor}
                   id="line-sparkline"
                   type="Line"
                   height="80px"
                   width="150px"
                   data={SparklineAreaData}
-                  color="blue"
+                  color={currentColor}
                 />
               </div>
 
               <div className="mt-10">
                 <Button 
                 color="white"
-                bgColor="blue"
+                bgColor={currentColor}
                 text="Download Report"
                 borderRadius="10px"
                 />

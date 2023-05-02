84657b79e83aa398ccd55bf3e2f5ff095998e6ad
Artur Vanivskyi <vanivskyi.artur@gmail.com>
2023-05-02 14:49:28 -0400

    fixed the bug with closing the shopping cart with value in Conmtext

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -15,56 +15,45 @@ export const ContextProvider = ({ children }) => {
   const [screenSize, setScreenSize] = useState(undefined);
   const [currentColor, setCurrentColor] = useState("#03C9D7");
   const [currentMode, setCurrentMode] = useState("Light");
-  const [themeSettings, setThemeSettings] = useState(false)
+  const [themeSettings, setThemeSettings] = useState(false);
 
   const setMode = (e) => {
     setCurrentMode(e.target.value);
 
     localStorage.setItem("themeMode", e.target.value);
 
-    setThemeSettings(false)
+    setThemeSettings(false);
   };
 
   const setColor = (color) => {
     setCurrentColor(color);
 
     localStorage.setItem("colorMode", color);
-    setThemeSettings(false)
+    setThemeSettings(false);
   };
 
-  const handleClick = (clicked) => {
-    setIsClicked((prevIsClicked) => ({
-      ...initialState,
-      [clicked]: !prevIsClicked[clicked],
-    }));
-    setIsClicked((prevIsClicked) => {
-      const newIsClicked = { ...prevIsClicked };
-      Object.keys(prevIsClicked).forEach((key) => {
-        if (key !== clicked) {
-          newIsClicked[key] = false;
-        }
-      });
-      return newIsClicked;
-    });
-  };
-
+  const handleClick = (clicked) =>
+    setIsClicked({ ...initialState, [clicked]: true });
 
   return (
     <StateContext.Provider
       value={{
-        activeMenu,
-        setActiveMenu,
-        isClicked,
-        setIsClicked,
-        handleClick,
-        screenSize,
-        setScreenSize,
         currentColor,
         currentMode,
+        activeMenu,
+        screenSize,
+        setScreenSize,
+        handleClick,
+        isClicked,
+        initialState,
+        setIsClicked,
+        setActiveMenu,
+        setCurrentColor,
+        setCurrentMode,
+        setMode,
+        setColor,
         themeSettings,
         setThemeSettings,
-        setColor,
-        setMode,
       }}
     >
       {children}
